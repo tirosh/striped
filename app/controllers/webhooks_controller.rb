7ad94af7e91197097a30acddf548d8c7b7452870
Till Grosch <mail@tillgrosch.com>
2023-12-21 20:46:37 +0100

    Handles subscription events

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -20,6 +20,10 @@ class WebhooksController < ApplicationController
     case event['type']
     when 'customer.subscription.created'
       handle_subscription_created(event['data']['object'])
+    when 'customer.subscription.deleted'
+      handle_subscription_deleted(event['data']['object'])
+    when 'invoice.payment_succeeded'
+      handle_payment_succeeded(event['data']['object'])
     end
 
     render json: { message: 'Success' }
@@ -34,5 +38,21 @@ class WebhooksController < ApplicationController
       stripe_subscription_id: subscription['id'],
       status: 'unpaid'
     )
+  end
+
+  def handle_subscription_deleted(subscription)
+    # Find the subscription in your database
+    local_subscription = Subscription.find_by(stripe_subscription_id: subscription.id)
+
+    # Update the status to 'canceled' if it's currently 'paid'
+    local_subscription.update(status: 'canceled') if local_subscription&.paid?
+  end
+
+  def handle_payment_succeeded(invoice)
+    # Find the subscription associated with the invoice
+    subscription = Subscription.find_by(stripe_subscription_id: invoice.subscription)
+
+    # Update the subscription status to 'paid' if it's currently 'unpaid'
+    subscription.update(status: 'paid') if subscription&.unpaid?
   end
 end
